0baef17137ac64f01286c516a52c765bbb052fe6
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2021-12-02 12:03:53 -0600

    Switch to Brewfile for Homebrew installs
    Install exa

---
+++ Brewfile
@@ -0,0 +1,16 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# Brews (Packages)
+brew "bat"
+brew "exa"
+brew "httpie"
+
+# Casks (Applications)
+cask "alfred"
+cask "google-chrome"
+cask "microsoft-auto-update"
+cask "microsoft-edge"
+cask "visual-studio-code"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,10 +4,4 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
-
-brew install --cask --no-quarantine google-chrome
-brew install --cask --no-quarantine visual-studio-code
-brew install --cask --no-quarantine microsoft-edge
-brew install --cask --no-quarantine alfred
+brew bundle --verbose 
