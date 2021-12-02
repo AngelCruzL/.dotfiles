81b2f0425c9bcfe9ee1797edb25aa2b7cc804c36
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2021-12-02 11:40:55 -0600

    Add setup_homebrew.zsh and run with Dotbot
    Install Homebrew
    Packages: httpie, bat
    Casks: Chrome, VS Code, Edge

---
+++ setup_homebrew.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install httpie
+brew install bat
+
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask microsoft-edge
