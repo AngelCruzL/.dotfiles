980244b4aa9b34e4779417b7d162786c86a425e7
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-29 00:20:37 -0500

    feat: adding global npm packages installation

--- setup_node.zsh
+++ setup_node.zsh
@@ -8,3 +8,15 @@ else
   echo "Installing Node with fnm"
   fnm install --lts
 fi
+
+# * Install Global NPM Packages
+
+echo "\n<<< Installing Global NPM Packages >>>\n"
+npm install --global npkill
+npm install --global typescript
+npm install --global json-server
+npm install --global http-server
+npm install --global @angular/cli
+
+echo "Global NPM Packages Installed"
+npm list --global --depth=0
