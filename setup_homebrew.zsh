d748363fb477734ad4547bb014518ac6646a09de
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-28 23:57:48 -0500

    feat: adding exists function

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,6 +2,11 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+  echo "Homebrew is already installed"
+else
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-brew bundle --verbose 
+brew bundle --verbose
