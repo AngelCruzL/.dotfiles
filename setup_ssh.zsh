c4fab53ea35315b82ffd618542f9b1989e78af55
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-10-10 23:46:04 -0600

    Update vps ssh connection key

--- setup_ssh.zsh
+++ setup_ssh.zsh
@@ -2,4 +2,4 @@
 
 ssh-keygen -t ed25519 -C "59263971+AngelCruzL@users.noreply.github.com" -f ~/.ssh/id_github
 ssh-keygen -t ed25519 -C "lcruz@expansive.mx" -f ~/.ssh/id_gitlab
-ssh-keygen -t ed25519 -C "langelcruzlara@hotmail.com" -f ~/.ssh/id_vps_root
+ssh-keygen -t ed25519 -C "langelcruzlara@hotmail.com" -f ~/.ssh/id_vps_angelcruz
