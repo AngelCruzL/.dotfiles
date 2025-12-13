ac093dbce23505a487d151340f4e2f9a529a7ca1
Ángel Cruz <me@angelcruzl.dev>
2025-12-13 17:41:19 -0600

    Update SSH key generation with new email for company key

--- setup-scripts/setup_ssh.zsh
+++ setup-scripts/setup_ssh.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 ssh-keygen -t ed25519 -C "59263971+AngelCruzL@users.noreply.github.com" -f ~/.ssh/id_github
-ssh-keygen -t ed25519 -C "company-key" -f ~/.ssh/id_company
+ssh-keygen -t ed25519 -C "me@angelcruzl.dev" -f ~/.ssh/id_company
 ssh-keygen -t ed25519 -C "vps-angelcruz" -f ~/.ssh/id_vps_angelcruz
